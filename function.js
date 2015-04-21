8f23ab1c83258e25ede2c9103b7fc926e622d56a
suneesh <suneeshtr@gmail.com>
2015-04-21 18:15:53 +0530

    Create function.js

---
+++ function.js
@@ -0,0 +1,13 @@
+function arrUnique(arr) {
+    var cleaned = [];
+    arr.forEach(function(itm) {
+        var unique = true;
+        cleaned.forEach(function(itm2) {
+            if (_.isEqual(itm, itm2)) unique = false;
+        });
+        if (unique)  cleaned.push(itm);
+    });
+    return cleaned;
+}
+
+var standardsList = arrUnique(standardsList);
